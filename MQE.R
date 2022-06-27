e7f9998f0feb3905296da9b01f0662896b43ad3e
Jiang "Kira" Shao <39611900+UWJiangShao@users.noreply.github.com>
2022-06-27 18:13:51 -0400

    Add files via upload

--- MQE.R
+++ MQE.R
@@ -1,5 +1,7 @@
 library(readxl)
 library(car)
+library(BSDA)
+library(dplyr)
 
 data.q4 <- read.csv("/Users/tigershao/Desktop/Duke/MQE/question_4.csv")
 
@@ -10,7 +12,7 @@ sd.trt <- sd(data.q4$Change[1:85])
 sd.pla <- sd(data.q4$Change[86:170])
 
 
-t.test(data.q4$Change[1:85], data.q4$Change[86:170])
+z.test(data.q4$Change[1:85], data.q4$Change[86:170])
 
 
 data.q3 <- read_excel(path = file.path("/Users/tigershao/Desktop/Duke/MQE", "DrivingdataAll.xls"))
@@ -26,8 +28,38 @@ hist(car2)
 hist(car3)
 hist(car4)
 
+
+fit.q3 <- lm(data.q3.mile.car1 ~ as.factor(data.q3$`OMR Version`) + data.q3$`Odom Reading 1 (Previous)`)
+summary(fit.q3)
+summary(aov(fit.q3))
+
+
+
+
+
+
 qqPlot(data.q3.mile.car1)
 hist(data.q3.mile.car1)
 
+trt <- data.q4$Change[1:85]
+pla <- data.q4$Change[86:170]
+
+t.test(trt, pla, sigma.x = sd.trt, sigma.y = sd.pla, alternative = "greater")
+
+############ Question 4
+trt_arm <- data.q4[1:85, ]
+pla_arm <- data.q4[86:170, ]
+
+
+ancova.q3.reg <- lm(data.q4$Followup ~ data.q4$AssignedTreatment + data.q4$Baseline)
+ancova_fit_1 <- aov(ancova.q3.reg)
+summary(ancova_fit_1)
+
+fit.q4 <- lm(data.q4$Change ~ data.q4$AssignedTreatment)
+summary(fit.q4)
+summary(aov(fit.q4))
+
+ancova.q5 <- aov(lm(data.q4$Change ~ data.q4$AssignedTreatment + data.q4$Baseline))
+summary(ancova.q5)
 
 
